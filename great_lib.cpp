7106bd65ff7982b2feeefc03daa3cdc0ebd56e82
sm <sebastian.molerus@gmail.com>
2023-01-17 13:25:29 +0100

    added great lib

---
+++ great_lib.cpp
@@ -0,0 +1,6 @@
+#include "great_lib.hpp"
+
+int somethingGreat(int a, int b)
+{
+    return a + b + 100;
+}
---
+++ great_lib.hpp
@@ -0,0 +1,6 @@
+#ifndef GREAT_LIB_HPP
+#define GREAT_LIB_HPP
+
+int somethingGreat(int a, int b);
+
+#endif
